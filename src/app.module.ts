27404e947bdb9d76e65d904c1ed7f5df2351c2f3
LF27819 <luciafeliperuiz@gmail.com>
2026-04-14 17:31:47 +0200

    Config PostgreSQL + Dcoker y TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AutenticacionModule } from './autenticacion/autenticacion.module';
 import { ServidoresModule } from './servidores/servidores.module';
@@ -6,8 +8,30 @@ import { CanalesModule } from './canales/canales.module';
 import { MensajesModule } from './mensajes/mensajes.module';
 
 @Module({
-  imports: [UsuariosModule, AutenticacionModule, ServidoresModule, CanalesModule, MensajesModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true
+      })
+    }),
+
+    UsuariosModule,
+    AutenticacionModule,
+    ServidoresModule,
+    CanalesModule,
+    MensajesModule
+  ],
 })
 export class AppModule {}
