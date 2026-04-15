7aa194b0aafbd73adfa6aa9fbf157b7752aaffa7
LF27819 <luciafeliperuiz@gmail.com>
2026-04-15 10:10:40 +0200

    Entidad usuarios - CRUD completo

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,25 +1,41 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { Usuario } from './entities/usuario.entity';
 
+
 @Controller('usuarios')
 export class UsuariosController {
-  constructor(private readonly usuariosService: UsuariosService) {}
+    constructor(private readonly usuariosService: UsuariosService) { }
 
-  @Get()
-  async obtenerUsuarios(): Promise<Usuario[]> {
-    return this.usuariosService.obtenerUsuarios();
-  }
+    @Get()
+    async obtenerUsuarios(): Promise<Usuario[]> {
+        return this.usuariosService.obtenerUsuarios();
+    }
 
-  @Get(':id')
-  async obtenerUsuarioPorId(
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<Usuario> {
-    return this.usuariosService.obtenerUsuarioPorId(id);
-  }
+    @Get(':id')
+    async obtenerUsuarioPorId(
+        @Param('id', ParseIntPipe) id: number,
+    ): Promise<Usuario> {
+        return this.usuariosService.obtenerUsuarioPorId(id);
+    }
 
-  @Post()
-  async crearUsuario(@Body() datosUsuario: Partial<Usuario>): Promise<Usuario> {
-    return this.usuariosService.crearUsuario(datosUsuario);
-  }
+    @Post()
+    async crearUsuario(@Body() datosUsuario: Partial<Usuario>): Promise<Usuario> {
+        return this.usuariosService.crearUsuario(datosUsuario);
+    }
+
+    @Put(':id')
+    async actualizarUsuario(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() datosUsuario: Partial<Usuario>,
+    ): Promise<Usuario> {
+        return this.usuariosService.actualizarUsuario(id, datosUsuario);
+    }
+
+    @Delete(':id')
+    async eliminarUsuario(
+        @Param('id', ParseIntPipe) id: number,
+    ): Promise<{ mensaje: string }> {
+        return this.usuariosService.eliminarUsuario(id);
+    }
 }
--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -8,7 +8,7 @@ export class UsuariosService {
   constructor(
     @InjectRepository(Usuario)
     private readonly usuarioRepository: Repository<Usuario>,
-  ) {}
+  ) { }
 
   async obtenerUsuarios(): Promise<Usuario[]> {
     return this.usuarioRepository.find();
@@ -27,5 +27,28 @@ export class UsuariosService {
   async crearUsuario(datosUsuario: Partial<Usuario>): Promise<Usuario> {
     const nuevoUsuario = this.usuarioRepository.create(datosUsuario);
     return this.usuarioRepository.save(nuevoUsuario);
+  }
+
+  async actualizarUsuario(id: number, datosUsuario: Partial<Usuario>): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOneBy({ id });
+
+    if (!usuario) {
+      throw new NotFoundException(`No se ha encontrado el usuario con id ${id}`);
+    }
+
+    const usuarioActualizado = Object.assign(usuario, datosUsuario);
+    return await this.usuarioRepository.save(usuarioActualizado);
+  }
+
+  async eliminarUsuario(id: number): Promise<{ mensaje: string }> {
+    const usuario = await this.usuarioRepository.findOneBy({ id });
+
+    if (!usuario) {
+      throw new NotFoundException(`No se ha encontrado el usuario con id ${id}`);
+    }
+
+    await this.usuarioRepository.remove(usuario);
+
+    return { mensaje: 'Usuario eliminado correctamente' };
   }
 }
